bc85697ea39e39f40e4a9d8f64accf84551311d7
LarsPresta <lauri.monni@gmail.com>
2024-03-06 11:50:39 +0100

    pageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,16 @@
+import styles from "./PageNotFound.module.css";
+import { NavLink } from "react-router-dom";
+import Footer from "../components/Footer";
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className={styles.pageNotFound}>
+      <PageNav />
+      <h1>404</h1>
+      <h2>Oops! It looks like this page doesn’t exist.</h2>
+      <NavLink to="/homepage">Go back to home page</NavLink>
+      <Footer />
     </div>
   );
 }
