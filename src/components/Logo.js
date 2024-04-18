c2e708843816eaac9cee4fd185de3d618934ce7b
LarsPresta <lauri.monni@gmail.com>
2024-04-18 11:02:34 +0200

    fix image path

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/homepage">
-      <img src="/logo.png" alt="Kasa logo" className={styles.logo} />
+      <img src="./logo.png" alt="Kasa logo" className={styles.logo} />
     </Link>
   );
 }
