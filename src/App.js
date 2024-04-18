2b55dd9b72c05c4007c90dba858cc07cbf9cccec
LarsPresta <lauri.monni@gmail.com>
2024-04-18 11:36:48 +0200

    styling and images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Accommodation from "./pages/Accommodation";
 import About from "./pages/About";
@@ -8,18 +8,15 @@ import accommodationsData from "./data/data.json";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Homepage accommodations={accommodationsData} />} />
         <Route path="homepage" element={<Homepage accommodations={accommodationsData} />} />
-        <Route
-          path="accommodation/:accommodationId"
-          element={<Accommodation accommodations={accommodationsData} />}
-        />
+        <Route path="accommodation/:accommodationId" element={<Accommodation accommodations={accommodationsData} />} />
         <Route path="about" element={<About />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <img src="/logo-white.png" alt="kasa logo"></img>
+      <img src={process.env.PUBLIC_URL + "/logo-white.png"} alt="kasa logo" />
       <p>&copy; Copyright {new Date().getFullYear()} Kasa. All rights reserved</p>
     </footer>
   );
